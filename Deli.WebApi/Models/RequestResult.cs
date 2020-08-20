65b71ec3cf7b09a7cd94c072190fa0794b8f793c
Ronchiqiao <51744129+Ronchiqiao@users.noreply.github.com>
2020-08-20 10:27:04 +0800

    132

--- Deli.WebApi/Models/RequestResult.cs
+++ Deli.WebApi/Models/RequestResult.cs
@@ -1,8 +1,4 @@
 /*add customized code between this region*/
-print("aaaaa")
-a=0 
-b=1
-c=a*b
 /*add customized code between this region*/
 namespace Deli.WebApi.Models
 {
@@ -11,15 +7,6 @@ namespace Deli.WebApi.Models
         public bool IsSucceed { get; set; }
         public string Message { get; set; }
         public object Data { get; set; }
-
-{
-    public class RequestResult
-    {
-        public bool IsSucceed { get; set; }
-        public string Message { get; set; }
-        public object Data { get; set;get; }
-
-
 /*add customized code between this region*/
 /*add customized code between this region*/
 
