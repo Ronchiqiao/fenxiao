0b6f62a4098770a9a640d52aed90c385abc398aa
Ronchiqiao <51744129+Ronchiqiao@users.noreply.github.com>
2020-08-20 10:24:12 +0800

    Update RequestResult.cs

--- Deli.WebApi/Models/RequestResult.cs
+++ Deli.WebApi/Models/RequestResult.cs
@@ -11,6 +11,15 @@ namespace Deli.WebApi.Models
         public bool IsSucceed { get; set; }
         public string Message { get; set; }
         public object Data { get; set; }
+
+{
+    public class RequestResult
+    {
+        public bool IsSucceed { get; set; }
+        public string Message { get; set; }
+        public object Data { get; set;get; }
+
+
 /*add customized code between this region*/
 /*add customized code between this region*/
 
